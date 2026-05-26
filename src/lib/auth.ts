c54f16c3864cadf0557acced643860928ac8738c
salman-como <salman.zahid@comosense.com>
2026-05-26 13:03:27 +0400

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+			clientId: process.env.AUTH_GOOGLE_ID!,
+			clientSecret: process.env.AUTH_GOOGLE_SECRET!,
 		}),
 	],
 	callbacks: {
